0567169d6dc03b1e8738884dec72b827b028beb3
Wallop Opasakhun (Windows) <champbassist159@gmail.com>
2025-06-19 23:14:22 +0700

    :sparkles: feat(ui): prepare data

---
+++ src/modules/portfolio.section/projects.ts
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    projectKey: "ezygas",
+    link: "https://ezygas.co",
+    tags: [
+      "nextjs",
+      "typescript",
+      "tailwindcss",
+      "django",
+      "django rest framework",
+      "postgresql",
+      "docker",
+      "docker compose",
+    ],
+  },
+  {
+    projectKey: "sol7",
+    link: "https://sol7.ai",
+    tags: ["nextjs", "typescript", "tailwindcss", "docker", "docker compose"],
+  },
+  {
+    projectKey: "selen",
+    link: "https://selen.ai",
+    tags: [
+      "nextjs",
+      "typescript",
+      "tailwindcss",
+      "docker",
+      "docker compose",
+      "sip.js",
+      "strapi",
+      "longdo map",
+    ],
+  },
+  {
+    projectKey: "synapes",
+    link: "https://synapes.ai",
+    tags: ["nextjs", "typescript", "tailwindcss", "docker", "docker compose", "strapi"],
+  },
+  {
+    projectKey: "senergy",
+    link: "https://synapes.ai",
+    tags: ["gatsbyjs", "typescript", "tailwindcss", "docker", "docker compose"],
+  },
+];
