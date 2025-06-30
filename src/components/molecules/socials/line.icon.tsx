7812a827924050bfaaada90ceff28aaa0e2b0532
Wallop Opasakhun (Windows) <champbassist159@gmail.com>
2025-06-30 20:25:14 +0700

    :bug: fix(components): accessibility fix for social links

--- src/components/molecules/socials/line.icon.tsx
+++ src/components/molecules/socials/line.icon.tsx
@@ -1,25 +1,25 @@
 import type { FC } from "react";
 
+import { line } from "@/config/links";
+
+import Base from "./base";
+
 export interface LineIconProps {
   size?: number;
 }
 const LineIcon: FC<LineIconProps> = ({ size = 50 }) => {
-  const onClick = () => {
-    window.open("https://line.me/ti/p/EldlUob9jm", "_blank");
-  };
   return (
-    <svg
-      onClick={onClick}
-      viewBox="0 0 170 170"
-      xmlns="http://www.w3.org/2000/svg"
-      className="cursor-pointer"
-      width={size}
-      height={size}
-      fill="none"
-    >
-      <rect x="0" y="0" width="170" height="170" rx="30" ry="30" fill="var(--icon-color)" />
-      <path
-        d="M147 78
+    <Base name="Add Line" link={line}>
+      <svg
+        viewBox="0 0 170 170"
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        fill="none"
+      >
+        <rect x="0" y="0" width="170" height="170" rx="30" ry="30" fill="var(--icon-color)" />
+        <path
+          d="M147 78
        c0-28-28-50-62-50.5
        -34 0-62 22.5-62 50.5
        0 25 22 45.5 52 50
@@ -29,55 +29,56 @@ const LineIcon: FC<LineIconProps> = ({ size = 50 }) => {
        -.5 1.5-1 6 5.5 3.5
        6.5-3 35.5-21 48.7-36
        9-10 13-20 13.3-31z"
-        fill="var(--background)"
-      />
+          fill="var(--background)"
+        />
 
-      <polyline
-        points="48.2,66.5 48.2,91.5 60.5,91.5"
-        stroke="var(--icon-color)"
-        strokeWidth="6.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill="none"
-      />
+        <polyline
+          points="48.2,66.5 48.2,91.5 60.5,91.5"
+          stroke="var(--icon-color)"
+          strokeWidth="6.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          fill="none"
+        />
 
-      <line
-        x1="70.2"
-        y1="66.5"
-        x2="70.2"
-        y2="91.5"
-        stroke="var(--icon-color)"
-        strokeWidth="6.5"
-        strokeLinecap="round"
-      />
+        <line
+          x1="70.2"
+          y1="66.5"
+          x2="70.2"
+          y2="91.5"
+          stroke="var(--icon-color)"
+          strokeWidth="6.5"
+          strokeLinecap="round"
+        />
 
-      <polyline
-        points="81.2,91.5 81.2,66.5 99.8,91.5 99.8,66.5"
-        stroke="var(--icon-color)"
-        strokeWidth="6.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill="none"
-      />
+        <polyline
+          points="81.2,91.5 81.2,66.5 99.8,91.5 99.8,66.5"
+          stroke="var(--icon-color)"
+          strokeWidth="6.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          fill="none"
+        />
 
-      <polyline
-        points="123.2,66.5 110.8,66.5 110.8,91.5 123.2,91.5"
-        stroke="var(--icon-color)"
-        strokeWidth="6.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill="none"
-      />
-      <line
-        x1="110.8"
-        y1="79"
-        x2="123.2"
-        y2="79"
-        stroke="var(--icon-color)"
-        strokeWidth="6.5"
-        strokeLinecap="round"
-      />
-    </svg>
+        <polyline
+          points="123.2,66.5 110.8,66.5 110.8,91.5 123.2,91.5"
+          stroke="var(--icon-color)"
+          strokeWidth="6.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          fill="none"
+        />
+        <line
+          x1="110.8"
+          y1="79"
+          x2="123.2"
+          y2="79"
+          stroke="var(--icon-color)"
+          strokeWidth="6.5"
+          strokeLinecap="round"
+        />
+      </svg>
+    </Base>
   );
 };
 
--- src/config/links.ts
+++ src/config/links.ts
@@ -3,6 +3,7 @@ export const linkedin = "https://www.linkedin.com/in/wallop-o-pasakhun";
 export const github = "https://github.com/champclarinet";
 export const email = "mailto:champbassist159@gmail.com";
 export const medium = "https://medium.com/@champbassist159";
+export const line = "https://line.me/ti/p/EldlUob9jm";
 export const react = "https://react.dev";
 export const synapes = "https://synapes.ai";
 export const nextjs = "https://nextjs.org";
